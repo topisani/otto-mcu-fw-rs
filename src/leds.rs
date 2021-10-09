621c9fba4a025895b1fe37985a58e892cbed1cbb
Tobias Pisani <tobias.pisani@moveinnovation.dk>
2021-10-09 22:25:48 +0200

    feat: led control

---
+++ src/leds.rs
@@ -0,0 +1,74 @@
+use embassy_stm32::spi::{Mode, Phase, Polarity};
+use rgb::RGB8;
+
+pub const MODE: Mode = Mode {
+    polarity: Polarity::IdleLow,
+    phase: Phase::CaptureOnFirstTransition,
+};
+
+pub struct Ws2812<SPI> {
+    spi: SPI,
+}
+
+// 
+
+impl<SPI> Ws2812<SPI>
+where
+    SPI: embedded_hal::blocking::spi::Write<u8>,
+{
+    
+    /// Use ws2812 devices via spi
+    ///
+    /// The SPI bus should run within 2 MHz to 3.8 MHz
+    pub fn new(spi: SPI) -> Self {
+        Self {
+            spi,
+        }
+    }
+    
+    /// Write a single byte for ws2812 devices
+    async fn write_byte(&mut self, mut data: u8) -> Result<(), SPI::Error> {
+        // Send two bits in one spi byte. High time first, then the low time
+        // The maximum for T0H is 500ns, the minimum for one bit 1063 ns.
+        // These result in the upper and lower spi frequency limits
+        let patterns = [0b0100_0100, 0b0100_0111, 0b0111_0100, 0b0111_0111];
+        for _ in 0..4 {
+            let bits = (data & 0b1100_0000) >> 6;
+            self.spi.write(&[patterns[bits as usize]])?;
+            data <<= 2;
+        }
+        Ok(())
+    }
+
+    async fn flush(&mut self) -> Result<(), SPI::Error> {
+        for _ in 0..20 {
+            self.spi.write(&[0])?;
+        }
+        Ok(())
+    }
+
+    /// Write all the items of an iterator to a ws2812 strip
+    pub async fn write<T, I>(&mut self, iterator: T) -> Result<(), SPI::Error>
+    where
+        T: Iterator<Item = I>,
+        I: Into<RGB8>,
+    {
+        // We introduce an offset in the fifo here, so there's always one byte in transit
+        // Some MCUs (like the stm32f1) only a one byte fifo, which would result
+        // in overrun error if two bytes need to be stored
+        // self.spi.write(0);
+        if cfg!(feature = "mosi_idle_high") {
+            self.flush().await?;
+        }
+
+        for item in iterator {
+            let item: RGB8 = item.into();
+            self.write_byte(item.g).await?;
+            self.write_byte(item.r).await?;
+            self.write_byte(item.b).await?;
+        }
+        self.flush().await?;
+        // Now, resolve the offset we introduced at the beginning
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,22 @@
 mod input;
 mod keys;
 mod util;
+mod leds;
 
+use defmt::{info, unwrap};
 use defmt_rtt as _;
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
-use embassy_stm32::gpio::{Level, Output, Pin,  Speed};
+use embassy_stm32::dma::NoDma;
+use embassy_stm32::gpio::{Level, Output, Pin, Speed};
 use embassy_stm32::pac::AFIO;
 use embassy_stm32::time::U32Ext;
-use embassy_stm32::{Config, Peripherals};
+use embassy_stm32::{peripherals, spi, Config, Peripherals};
 use embedded_hal::digital::v2::OutputPin;
 use keys::KeyMatrix;
 // global logger
 use panic_probe as _;
-
-pub use defmt::*;
+use rgb::RGB8;
 
 use core::sync::atomic::{AtomicUsize, Ordering};
 
@@ -35,7 +37,6 @@ defmt::timestamp! {
     }
 }
 
-
 fn config() -> Config {
     let mut config = Config::default();
     config.rcc.hse = Some(16.mhz().into());
@@ -45,14 +46,30 @@ fn config() -> Config {
     config.rcc.pclk2 = Some(48.mhz().into());
     config.rcc.adcclk = Some(12.mhz().into());
     config
+
+}
+
+type Leds = leds::Ws2812<spi::Spi<'static, peripherals::SPI1, NoDma, NoDma>>;
+
+#[embassy::task]
+async fn test_leds(mut leds: Leds) {
+    let mut colors = [RGB8::default(); 54];
+    loop {
+        for i in 0..colors.len() {
+            colors[i] = RGB8::new(0xFF, 00, 0x20);
+            leds.write(colors.clone().into_iter()).await.unwrap();
+            Timer::after(Duration::from_millis(300)).await;
+        }
+        for i in 0..colors.len() {
+            colors[i] = RGB8::new(0, 0, 0);
+            leds.write(colors.clone().into_iter()).await.unwrap();
+            Timer::after(Duration::from_millis(300)).await;
+        }
+    }
 }
 
 #[embassy::main(config = "config()")]
 async fn main(spawner: Spawner, p: Peripherals) {
-    // We use PB3 and PB4 for the keyboard matrix, so disable JTAG (keeping SWD enabled).
-    unsafe {
-        AFIO.mapr().modify(|m| m.set_swj_cfg(010u8));
-    }
     let km = KeyMatrix::new(
         [
             p.PC9.degrade(),
@@ -67,7 +84,8 @@ async fn main(spawner: Spawner, p: Peripherals) {
         [
             p.PB11.degrade(),
             p.PB0.degrade(),
-            p.PA6.degrade(),
+            p.PC1.degrade(), // Not right
+            // p.PA6.degrade(),
             p.PC4.degrade(),
             p.PC5.degrade(),
             p.PB10.degrade(),
@@ -76,7 +94,28 @@ async fn main(spawner: Spawner, p: Peripherals) {
         ],
     );
 
-    spawner.spawn(input::poll_input(km)).unwrap();
+    let mut spi_config = spi::Config::default();
+    spi_config.mode = leds::MODE;
+    let spi = spi::Spi::new(
+        p.SPI1,
+        p.PA5,
+        p.PA7,
+        p.PA6,
+        NoDma,
+        NoDma,
+        3.mhz(),
+        spi_config,
+    );
+    
+    // We use PB3 and PB4 for the keyboard matrix, so disable JTAG (keeping SWD enabled).
+    unsafe {
+        AFIO.mapr().modify(|m| m.set_swj_cfg(010u8));
+    }
+
+    let leds = leds::Ws2812::new(spi);
+
+    unwrap!(spawner.spawn(input::poll_input(km)));
+    unwrap!(spawner.spawn(test_leds(leds)));
     let mut led = Output::new(p.PC6, Level::High, Speed::Low);
 
     loop {
